47f59b265eee54d1037eca43f9d8ddf9e2f5592d
Haley Jeppson <hjeppson@iastate.edu>
2020-02-08 15:57:46 -0600

    updated data

--- R/data.R
+++ R/data.R
@@ -97,83 +97,6 @@
 #' @format A data frame with 62466 rows and 11 variables
 "happy"
 
-#' Box office data
-#'
-#' The data contains weekly box office numbers as published on the website 
-#' https://www.the-numbers.com/weekly-box-office-chart scraped on 
-#' Oct 16 2019.
-#'
-#' \itemize{
-#'  \item Rank current rank of the movie according to gross box office
-#'  \item Rank.Last.Week last week's ranking of box office gross
-#'  \item Movie name of the movie
-#'  \item Distributor name of the Distributor
-#'  \item Gross weekly box office gross in US dollars 
-#'  \item Change percent change in gross from last week
-#'  \item Thtrs. number of movie theaters the movie is being shown
-#'  \item Per.Thr. per theater gross
-#'  \item Total.Gross cumulative box office gross in 100 million US dollars 
-#'  \item Week number of weeks a movie has been shown
-#'  \item Date date of the publication of box office numbers
-#' }
-#'
-#' @keywords datasets
-#' @name box
-#' @usage data(box)
-#' @format A data frame with 32885 rows and 11 variables
-"box"
-
-
-#' Movie budget data
-#'
-#' The data contains movie budgets and box office gross for the 5000 top grossing movies (as of May 13 2018) published 
-#' on https://www.the-numbers.com/movie/budgets/all/1
-#'
-#' \itemize{
-#'  \item Release Date (date) 
-#'  \item Movie name of the movie
-#'  \item Production Budget (dbl) budget in US dollars 
-#'  \item Domestic Gross (dbl) in US dollars 
-#'  \item Worldwide Gross (dbl) in US dollars 
-#' }
-#'
-#' @keywords datasets
-#' @name budget
-#' @usage data(budget)
-#' @format A data frame with 5855 rows and 5 variables
-"budget"
-
-
-#' Box office data from the Mojo website
-#'
-#' The data contains weekly box office numbers as published on the website 
-#' https://www.boxofficemojo.com/weekend/chart/ scraped on 
-#' Sep 10 2018.
-#'
-#' \itemize{
-#'  \item TW rank this week
-#'  \item LW rank last week
-#'  \item Title name of the movie
-#'  \item Studio name of the producing studio
-#'  \item Weekend Gross weekend box office gross in US dollars 
-#'  \item `% Change`` percent change in weekend gross from last week
-#'  \item Theater Count number of movie theaters the movie is being shown
-#'  \item Theater Change change in the number of theaters the movie was shown
-#'  \item Average average gross per theater 
-#'  \item Total Gross cumulative box office gross in US dollars 
-#'  \item Budget (in Million) estimated budget
-#'  \item Week week that the movie has been in theaters
-#'  \item Weekend character string of the weekend of the show date
-#'  \item Year integer of the year of the show date (between 2013 and 2018)
-#'  \item WeekNo integer, week number of the year (1 through 52 or 53) 
-#' }
-#'
-#' @keywords datasets
-#' @name mojo
-#' @usage data(mojo)
-#' @format A data frame with 31718 rows and 15 variables.
-"mojo"
-
 #' Passengers and crew on board the Titanic
 #'
 #' A dataset containing some demographics and survival of people on board the Titanic
@@ -185,6 +108,8 @@
 #'   \item{Survived}{Yes/No.}
 #' }
 #' @keywords datasets
+#' @name titanic
+#' @usage data(titanic)
 "titanic"
 
 #' Earthquake data
@@ -222,4 +147,37 @@
 #' @format A data frame with 22 variables
 "earthquakes"
 
-
+#' Iowa alcohol sales
+#'
+#' A dataset containing liquor sales?
+#' @format A data frame with 466126 rows and 24 variables:
+#' \describe{
+#'   \item{Invoice/Item Number}{Invoice number}
+#'   \item{Date}{Date of sale}
+#'   \item{Store Number}{store identifier}
+#'   \item{Store Name}{name of store}
+#'   \item{Address}{address of store}
+#'   \item{City}{city location of store}
+#'   \item{Zip Code}{zip code of store}
+#'   \item{Store Location}{long and lat of store}
+#'   \item{County Number}{county number of store}
+#'   \item{County}{county of store}
+#'   \item{Category}{Category number of liquor}
+#'   \item{Category Name}{Category name of liquor}
+#'   \item{Vendor Number}{number of vendor}
+#'   \item{Vendor Name}{name of vendor}
+#'   \item{Item Number}{item number}
+#'   \item{Item Description}{item description}
+#'   \item{Pack}{size of pack}
+#'   \item{Bottle Volume (ml)}{volume size}
+#'   \item{State Bottle Cost}{cost of item}
+#'   \item{State Bottle Retail}{retail cost of item}
+#'   \item{Bottles Sold}{number of bottles sold}
+#'   \item{Sale (Dollars)}{sales}
+#'   \item{Volume Sold (Liters)}{volume sold in liters}
+#'   \item{Volume Sold (Gallons)}{volume sold in gallons}
+#' }
+#' @keywords datasets
+#' @name alcohol
+#' @usage data(alcohol)
+"alcohol"
